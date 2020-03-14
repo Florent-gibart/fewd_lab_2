4d90316024d2c57a630fad69a2c64fedb7b63684
Florent-gibart <florent.gibart@epita.fr>
2020-03-14 17:55:09 +0000

    commit

---
+++ fewd_lab_2_doc.js
@@ -0,0 +1,57 @@
+Florent Gibart 22477 
+
+3. This array has a length of 7.
+
+4. 
+function myAlphabetLength(array)
+{
+    return console.log(array.length);
+};
+
+5. 
+var Planets;
+Planets = ['Mars', 'Uranus', 'Earth', 'Pluto', 'Venus']
+
+6.
+console.log(Planets[0], Planets[1], Planets[2], Planets[3], Planets[4]);
+
+7.
+
+8.
+    15
+    -5
+    50
+    2
+    1
+    6
+    4
+
+9.
+    5Hello
+    Hello World
+    15
+
+10.
+    true
+    false
+    true
+    true
+    false
+    true
+    false
+    true
+    true
+    true
+
+11.
+var hello;
+
+var hello = function(){
+    return 'Hello World';
+};
+
+12.
+console.log(hello);
+
+13.
+X has the value : 'Hi!'.
